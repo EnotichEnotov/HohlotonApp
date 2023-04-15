38055b65445efc72438be9150a021c2938d7f9c0
C_Bits <lankdaroi@gmail.com>
2023-04-15 21:08:58 +0300

    уу

---
+++ app/src/main/java/com/example/figmamc/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.figmamc;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_sign_up);
+    }
+}
